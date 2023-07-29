a55bebf37e0f8f4b0ff5d3229f5a26354a7d6c37
William Blake Galbreath <Blake.Galbreath@GMail.com>
2023-07-28 21:59:17 -0500

    remove left over debug spam

--- src/Server/PlayerListServer.cs
+++ src/Server/PlayerListServer.cs
@@ -23,21 +23,16 @@ public class PlayerListServer {
         foreach (IServerPlayer player in api.World.AllOnlinePlayers.Cast<IServerPlayer>()) {
             packet.pings.Add(player.PlayerUID, player.Ping);
         }
-        api.Logger.Event("Sending packet: " + packet);
         if (packet == null) {
-            api.Logger.Event("Null packet");
             return;
         }
         if (packet.pings == null) {
-            api.Logger.Event("Null pings");
             return;
         }
         if (packet.pings.Count == 0) {
-            api.Logger.Event("Empty pings");
             return;
         }
         if (networkHandler == null) {
-            api.Logger.Event("Null networkHandler");
             return;
         }
         networkHandler.SendPacket(packet);
