aa8fa3a3b4648b5ec00a0f08be49ac284577d583
William Blake Galbreath <blake.galbreath@gmail.com>
2025-11-10 16:51:51 -0600

    simplify icon code

--- src/gui/PingIcon.cs
+++ src/gui/PingIcon.cs
@@ -1,49 +1,29 @@
 using Vintagestory.API.Client;
 using Vintagestory.API.Common;
+using Vintagestory.API.Util;
 
 namespace playerlist.gui;
 
 public class PingIcon {
-    private readonly BitmapRef _0;
-    private readonly BitmapRef _1;
-    private readonly BitmapRef _2;
-    private readonly BitmapRef _3;
-    private readonly BitmapRef _4;
+    private readonly BitmapRef[] _icon = new BitmapRef[5];
 
     public PingIcon(ICoreClientAPI capi) {
-        _0 = capi.Assets.Get(new AssetLocation("playerlist", "textures/ping_0.png")).ToBitmap(capi);
-        _1 = capi.Assets.Get(new AssetLocation("playerlist", "textures/ping_1.png")).ToBitmap(capi);
-        _2 = capi.Assets.Get(new AssetLocation("playerlist", "textures/ping_2.png")).ToBitmap(capi);
-        _3 = capi.Assets.Get(new AssetLocation("playerlist", "textures/ping_3.png")).ToBitmap(capi);
-        _4 = capi.Assets.Get(new AssetLocation("playerlist", "textures/ping_4.png")).ToBitmap(capi);
+        for (int i = 0; i < 5; i++) {
+            _icon[i] = capi.Assets.Get(new AssetLocation("playerlist", $"textures/ping_{i}.png")).ToBitmap(capi);
+        }
     }
 
     public BitmapRef Get(int[]? thresholds, int ping) {
-        if (thresholds == null || ping < 0) {
-            return _0;
-        }
-
-        if (ping <= thresholds[0]) {
-            return _4;
-        }
-
-        if (ping <= thresholds[1]) {
-            return _3;
-        }
-
-        // ReSharper disable once ConvertIfStatementToReturnStatement
-        if (ping <= thresholds[2]) {
-            return _2;
-        }
-
-        return _1;
+        return ping switch {
+            _ when ping < 0 || thresholds == null => _icon[0], // 0 bars
+            _ when ping < thresholds[0] => _icon[4], // 4 bars
+            _ when ping < thresholds[1] => _icon[3], // 3 bars
+            _ when ping < thresholds[2] => _icon[2], // 2 bars
+            _ => _icon[1] // 1 bar
+        };
     }
 
     public void Dispose() {
-        _0.Dispose();
-        _1.Dispose();
-        _2.Dispose();
-        _3.Dispose();
-        _4.Dispose();
+        _icon.Foreach(icon => icon.Dispose());
     }
 }
